7bc3bf1d3f5966744448b537294508524004ff4e
James Smith <james@floppy.org.uk>
2024-12-11 19:17:17 +0000

    create buffers for meshes

--- lib/mittsu/gltf/exporter.rb
+++ lib/mittsu/gltf/exporter.rb
@@ -1,11 +1,20 @@
 require "jbuilder"
+require "base64"
 
 module Mittsu
   class GLTFExporter
     def initialize(options = {})
+      @buffers = []
+      @meshes = {}
     end
 
-    def export(_object, filename)
+    def export(object, filename)
+      object.traverse do |obj|
+        if obj.is_a? Mittsu::Mesh
+          @meshes[obj.uuid] ||= {}
+          @meshes[obj.uuid][:buffer_index] = add_buffer(obj)
+        end
+      end
       File.write(
         filename,
         Jbuilder.new do |json|
@@ -18,15 +27,36 @@ module Mittsu
             nodes: []
           }]
           json.nodes []
-          json.meshes []
-          json.buffers []
-          json.bufferViews []
-          json.accessors []
+          json.buffers do
+            json.array! @buffers
+          end
         end.target!
       )
     end
 
     # Parse is here for consistency with THREE.js's weird naming of exporter methods
     alias_method :parse, :export
+
+    private
+
+    def add_buffer(mesh)
+      index = @buffers.count
+      # Pack faces into an array
+      pack_string = (mesh.geometry.faces.count > (2**16)) ? "L<*" : "S<*"
+      faces = mesh.geometry.faces.map { |x| [x.a, x.b, x.c] }
+      data = faces.flatten.pack(pack_string)
+      # Add padding to get to integer multiple of float size
+      padding = 4 - (data.length % 4)
+      data += Array.new(padding, 0).pack("C*")
+        # Pack vertices in as floats
+      vertices = mesh.geometry.vertices.map(&:elements)
+      data += vertices.flatten.pack("f*")
+      # Encode and store in buffers
+      @buffers << {
+        uri: "data:application/octet-stream;base64," + Base64.encode64(data).strip,
+        byteLength: data.length
+      }
+      index
+    end
   end
 end
