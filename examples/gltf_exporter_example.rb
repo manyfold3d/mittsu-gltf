c2a2c3a74da25ff52085b9b6db4f772f3112685b
James Smith <james@floppy.org.uk>
2024-12-20 12:52:05 +0000

    change sample file used for examples

--- examples/gltf_exporter_example.rb
+++ examples/gltf_exporter_example.rb
@@ -2,9 +2,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "mittsu/gltf"
 
 loader = Mittsu::OBJLoader.new
-object = loader.load(File.expand_path("./mittsu.obj", File.dirname(__FILE__)))
+object = loader.load(File.expand_path("./gltf.obj", File.dirname(__FILE__)))
 
 exporter = Mittsu::GLTFExporter.new
-exporter.export(object, File.expand_path("./mittsu-export.gltf", File.dirname(__FILE__)))
+exporter.export(object, File.expand_path("./export.gltf", File.dirname(__FILE__)))
 
-exporter.export(object, File.expand_path("./mittsu-export.glb", File.dirname(__FILE__)), mode: :binary)
+exporter.export(object, File.expand_path("./export.glb", File.dirname(__FILE__)), mode: :binary)
